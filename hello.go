e5fc0096f25a72a1dc862db8cf37f6ff259f1695
Abel Koech <koechabel161@gmail.com>
2024-11-11 09:41:58 +0300

    intro to Go

---
+++ hello.go
@@ -0,0 +1,19 @@
+// Learning Go
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello, Abel!! This is your fast code in Go.")
+}
+
+// package main
+
+// import "fmt"
+
+// import "rsc.io/quote"
+
+// func main() {
+//     fmt.Println(quote.Go())
+// }
